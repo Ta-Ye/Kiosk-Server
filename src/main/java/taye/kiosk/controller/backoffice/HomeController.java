2dc336f4bb5b246b60535251fa82a4018bd1f941
Ta-Ye <cty1272@naver.com>
2021-05-15 02:04:55 +0900

    연령별 주문정보

--- src/main/java/taye/kiosk/controller/backoffice/HomeController.java
+++ src/main/java/taye/kiosk/controller/backoffice/HomeController.java
@@ -1,5 +1,9 @@
 package taye.kiosk.controller.backoffice;
 
+import java.util.List;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +11,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
 import taye.kiosk.domain.Menu;
+import taye.kiosk.domain.Store;
+import taye.kiosk.dto.StoreDTO.StoreDetail;
 import taye.kiosk.service.MenuService;
+import taye.kiosk.service.OrderService;
+import taye.kiosk.service.StoreService;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 	
 	private final MenuService menuService;
+	private final OrderService orderService;
+	private final StoreService storeService;
 	
 	@GetMapping("/age")
-	public String charts() {
+	public String charts(Model model) {
+		StoreDetail storeDetail = (StoreDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Store store = storeService.findStore(storeDetail.getStoreName()).orElseThrow(() -> new UsernameNotFoundException(storeDetail.getStoreName()));
+		List<Menu> menus = menuService.getAllMenuList();
+		List<String> menuNames = orderService.getMenuNames(menus);
+		
+		model.addAttribute("menuNames", menuNames);
+		model.addAttribute("sales", orderService.getOrdersPriceByAgeNum(orderService.getOrdersByAge(store,menus), menuNames));
+		model.addAttribute("prices", orderService.getOrdersPriceByAgeNum(orderService.getOrderPriceByAge(store,menus), menuNames));
 		return "charts";
 	}
 	
--- src/main/java/taye/kiosk/service/OrderService.java
+++ src/main/java/taye/kiosk/service/OrderService.java
@@ -2,7 +2,9 @@ package taye.kiosk.service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,61 +18,125 @@ import taye.kiosk.dto.OrderDTO.OrderSale;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository orderRepo;
-	
+
 	public Order insertOrder(List<Menu> menus, Store store, String age) {
 		return orderRepo.save(Order.builder().age(age).orderDate(LocalDate.now()).store(store).menus(menus).build());
 	}
-	
+
 	private List<Order> getSortedOrders(Store store, LocalDate date) {
 		List<Order> orders = orderRepo.findMonthlyOrders(store.getStoreNum(), date);
-		orders.sort((a,b) -> a.getOrderDate().compareTo(b.getOrderDate()));
+		orders.sort((a, b) -> a.getOrderDate().compareTo(b.getOrderDate()));
 		return orders;
 	}
-	
+
+	private Map<String, Integer> getMenuMap(List<Menu> menus) {
+
+		Map<String, Integer> menuMap = new HashMap<>();
+		for (Menu menu : menus) {
+			menuMap.put(menu.getMenuName(), 0);
+		}
+		return menuMap;
+	}
+
+	private Map<String, Map<String, Integer>> getMenuAgeMap(List<Menu> menus) {
+
+		Map<String, Map<String, Integer>> menuMap = new HashMap<>();
+		menuMap.put("young", getMenuMap(menus));
+		menuMap.put("middle", getMenuMap(menus));
+		menuMap.put("old", getMenuMap(menus));
+		return menuMap;
+	}
+
 	public List<OrderSale> getOrdersPerDay(Store store) {
-		
+
 		LocalDate date = LocalDate.now();
 		List<Order> orders = getSortedOrders(store, LocalDate.of(date.getYear(), date.getMonth(), 1));
-		
+
 		int[] days = new int[31];
-		orders.forEach(o -> days[o.getOrderDate().getDayOfMonth()-1]++);
-		
+		orders.forEach(o -> days[o.getOrderDate().getDayOfMonth() - 1]++);
+
 		List<OrderSale> ans = new ArrayList<>();
-		for (int i=0 ; i<31; i++) {
-			if (days[i]>0) {
-				ans.add(new OrderSale(i+1, days[i]));
+		for (int i = 0; i < 31; i++) {
+			if (days[i] > 0) {
+				ans.add(new OrderSale(i + 1, days[i]));
 			}
 		}
 		return ans;
 	}
-	
+
 	public int[] getSalesPerMonth(Store store) {
-		
+
 		LocalDate date = LocalDate.now();
 		List<Order> orders = getSortedOrders(store, LocalDate.of(date.getYear(), 1, 1));
-		
+
 		int[] months = new int[12];
 		orders.forEach(o -> {
 			int sum = 0;
 			for (Menu menu : o.getMenus()) {
-				sum+=menu.getPrice();
+				sum += menu.getPrice();
 			}
-			months[o.getOrderDate().getMonthValue()-1]+=sum;
+			months[o.getOrderDate().getMonthValue() - 1] += sum;
 		});
-		
+
 		return months;
 	}
-	
+
 	public List<OrderDetail> getOrdersByYear(Store store) {
-		
+
 		List<OrderDetail> details = new ArrayList<>();
 		LocalDate date = LocalDate.now();
-		for (Order order : getSortedOrders(store, LocalDate.of(date.getYear(), 1, 1))){
+		for (Order order : getSortedOrders(store, LocalDate.of(date.getYear(), 1, 1))) {
 			details.add(OrderDetail.of(order));
 		}
 		return details;
+	}
+
+	public Map<String, Map<String, Integer>> getOrdersByAge(Store store, List<Menu> menus) {
+
+		Map<String, Map<String, Integer>> menuMap = getMenuAgeMap(menus);
+		for (Order order : getSortedOrders(store, LocalDate.of(LocalDate.now().getYear(), 1, 1))) {
+			for (Menu menu : order.getMenus()) {
+				menuMap.get(order.getAge()).replace(menu.getMenuName(),
+						menuMap.get(order.getAge()).get(menu.getMenuName()) + 1);
+			}
+		}
+		return menuMap;
+	}
+
+	public Map<String, Map<String, Integer>> getOrderPriceByAge(Store store, List<Menu> menus) {
+
+		Map<String, Map<String, Integer>> menuMap = getMenuAgeMap(menus);
+		for (Order order : getSortedOrders(store, LocalDate.of(LocalDate.now().getYear(), 1, 1))) {
+			for (Menu menu : order.getMenus()) {
+				menuMap.get(order.getAge()).replace(menu.getMenuName(),
+						menuMap.get(order.getAge()).get(menu.getMenuName()) + menu.getPrice());
+			}
+		}
+		return menuMap;
+	}
+	
+	public List<String> getMenuNames(List<Menu> menus) {
+		
+		List<String> menuNames = new ArrayList<>();
+		menus.forEach(m -> menuNames.add(m.getMenuName()));
+		return menuNames;
+	}
+	
+	public Map<String, List<Integer>> getOrdersPriceByAgeNum(Map<String, Map<String, Integer>> maps, List<String> menuNames) {
+		
+		Map<String, List<Integer>> mapNums = new HashMap<>();
+
+		mapNums.put("young", new ArrayList<>());
+		mapNums.put("middle", new ArrayList<>());
+		mapNums.put("old", new ArrayList<>());
+		for (String name : menuNames) {
+			mapNums.get("young").add(maps.get("young").get(name));
+			mapNums.get("middle").add(maps.get("middle").get(name));
+			mapNums.get("old").add(maps.get("old").get(name));
+		}
+		return mapNums;
 	}
 }
